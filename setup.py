5614383de274b92505f62e7247a0544c045ecc41
kodamaChameleon <bryant.a.renfroe@gmail.com>
2023-08-08 08:53:02 -0400

    Remove unnecessary function

--- setup.py
+++ setup.py
@@ -8,29 +8,7 @@ def install_requirements():
 def build_config():
     subprocess.check_call(['python3', 'project.py', 'list'])
 
-# Store Twilio API creds
-def store_account_credentials():
-
-    if os.path.exists(".key"):
-        answer = ""
-        while answer not in ['y','n']:
-            answer = input(".key already exist. Overwrite (y/n)? ").lower() 
-
-        if answer == 'n':
-            return
-
-    account_sid = input("Please enter your Twilio Account SID: ")
-    auth_token = input("Please enter your Twilio Auth Token: ")
-
-    try:
-        with open(".key", "w") as file:
-            file.write(f"ACCOUNT_SID={account_sid}\n")
-            file.write(f"AUTH_TOKEN={auth_token}\n")
-        print("Account credentials have been successfully stored in .key.")
-    except Exception as e:
-        print(f"An error occurred while storing the credentials: {e}")
 
 if __name__ == '__main__':
     install_requirements()
-    store_account_credentials()
     build_config()
